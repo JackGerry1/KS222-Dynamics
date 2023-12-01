281606165376714c2cae436730f6aab6a74006a4
Davy Tregellas <davy.tregellas@students.plymouth.ac.uk>
2023-12-01 19:05:35 +0000

    title_change

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+      <h2>Welcome to the KS222 Dynamics</h2>
         {/* Navigation bar with links */}
         <nav>
           <ul>
